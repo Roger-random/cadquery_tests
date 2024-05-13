eafc5cd5e10aa8faf83c06c3dd9e7f5ca8fc7f6f
Roger Cheng <roger.random@outlook.com>
2024-05-13 01:41:05 -0700

    WIP: single clear LED pointed the wrong way

--- usb_led_00_minimalist/usb_led_00_minimalist.py
+++ usb_led_00_minimalist/usb_led_00_minimalist.py
@@ -12,7 +12,18 @@ reflector_depth_front = 7.15
 reflector_depth_rear = 1.8
 
 led_diameter = 5
+led_base_diameter = 5.3
+led_base_height = 1.0
+led_overall_height = 8.7
+
 led_ring_diameter = 15.2
+
+led_pcb_thickness = 1.25
+led_pcb_diameter = 23.5
+
+color_clear_plastic = cq.Color(1,1,1,0.2)
+color_pcb = cq.Color(0,0.5,0)
+color_mirror_finish = cq.Color(0.7, 0.7, 0.7)
 
 def make_front_window():
     fw = cq.Workplane("XZ")
@@ -44,12 +55,41 @@ def make_reflector():
 
     return r
 
+def make_led():
+    r = cq.Workplane("XY")
+    r = r.lineTo(led_base_diameter/2,0)
+    r = r.lineTo(led_base_diameter/2,led_base_height)
+    r = r.lineTo(led_diameter/2,led_base_height)
+    r = r.lineTo(led_diameter/2,led_overall_height-(led_diameter/2))
+    r = r.radiusArc((0,led_overall_height),-led_diameter/2)
+    r = r.close()
+    r = r.revolve()
+
+    return r
+
+def make_led_ring():
+    lr = cq.Assembly()
+    lr = lr.add(
+        (
+            cq.Workplane("XZ")
+            .circle(led_pcb_diameter/2)
+            .extrude(led_pcb_thickness)
+        ),
+        name = "led_pcb",
+        color = color_pcb)
+    lr = lr.add(
+        make_led(),
+        name = "led_center",
+        color = color_clear_plastic)
+
+    return lr
+
 la = cq.Assembly()
 offset_y = window_thickness
 la = la.add(
     make_front_window(),
     name = "window",
-    color = cq.Color(1,1,1,0.2),
+    color = color_clear_plastic,
     loc = cq.Location((0,offset_y,0)))
 
 offset_y += exploded_view_spacer
@@ -57,7 +97,7 @@ offset_y += reflector_thickness
 la = la.add(
     make_reflector(),
     name = "reflector",
-    color = cq.Color(0.9, 0.9, 0.9),
+    color = color_mirror_finish,
     loc = cq.Location((0,offset_y,0)))
 
-show_object(la)
+show_object(make_led_ring())
