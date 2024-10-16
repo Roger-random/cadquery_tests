8395dfdac39bac2b13b3a38a8a6fda4fd4851b0c
Roger Cheng <roger.random@outlook.com>
2024-10-16 01:58:14 -0700

    Giving up on sketch, use ellipseArc()

--- spent_spool_tray.py
+++ spent_spool_tray.py
@@ -14,11 +14,21 @@ additional_clearance = 0.25
 # Mitigate 3D printing elephant foot effect
 elephant_foot_compensation = 0.5
 
-# Calculate from circumference
-inner_radius = 281 / (math.pi*2)
+# How big of a wedge we want in degrees
+angle = 30
 
-# Outer diameter is easier to measure
-outer_radius = 200 / 2
+# Calculate spool inner radius from measuring circumference of spool center
+spool_inner_circumference = 281
+inner_radius = spool_inner_circumference / (math.pi*2)
+
+# Outer spool diameter is easier to measure directly
+spool_outer_diameter = 200
+spool_outer_radius = spool_outer_diameter / 2
+
+# Tray actually extends beyond edge of spool
+outer_radius = spool_outer_radius + 10
+
+height = 56
 
 base = (
     cq.Workplane("XY")
@@ -27,5 +37,13 @@ base = (
     .extrude(1)
     )
 
-show_object(base)
+wedge = (
+    cq.Workplane("XY")
+    .lineTo(outer_radius,0)
+    .ellipseArc(outer_radius,outer_radius,0,angle,0)
+    .close()
+    .extrude(height)
+    )
+
+show_object(wedge+base)
 
