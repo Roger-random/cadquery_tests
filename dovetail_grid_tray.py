9bda92d2813be82fa8e96b40acaeb9e91056addc
Roger Cheng <roger.random@outlook.com>
2024-10-31 00:44:37 -0700

    Graduated to https://github.com/Roger-random/storage_grid

--- dovetail_grid_tray.py
+++ dovetail_grid_tray.py
@@ -1,12 +1,3 @@
-import math
-import cadquery as cq
-
-from dovetailstoragegrid import DovetailStorageGrid as dsg
-
-dsg_01 = dsg(20, 15, 25)
-
-show_object(dsg_01.label_tray(1,1), options = {"alpha":0.5, "color":"blue"})
-
-show_object(dsg_01.label_tray(1,1).translate((20,0,0)), options = {"alpha":0.5, "color":"red"})
-
-show_object(dsg_01.label_tray(1,1).translate((0,15,0)), options = {"alpha":0.5, "color":"green"})
+"""
+Moved to https://github.com/Roger-random/storage_grid
+"""
