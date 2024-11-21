cdf7e24e2882eb7864061efff04f306b15207e21
Roger Cheng <roger.random@outlook.com>
2024-11-21 12:53:20 -0800

    608 bearing block tilted at various angles

---
+++ bearing_block_608_angled.py
@@ -0,0 +1,35 @@
+"""
+Generate a series of blocks for generic 608 bearings, combing
+them together into a single test piece.
+
+Originally created to see if support material generation is
+good enough to allow bearings to be used at arbitrary angles
+in a 3D printed device.
+"""
+import cadquery as cq
+
+def bearing_block():
+    return (
+        cq.Workplane("XY")
+        .polygon(8, 30, circumscribed=True)
+        .circle(5)
+        .extrude(20)
+        .faces(">Z").workplane()
+        .circle(11)
+        .extrude(-7, combine="cut")
+        .faces("<Z").workplane()
+        .circle(11)
+        .extrude(-7, combine="cut")
+        .translate((15,15,0))
+        )
+
+big_block = bearing_block()
+
+for block in range(1,7):
+    big_block = big_block + (
+        bearing_block()
+        .translate((0,block*30,0))
+        .rotate((0,0,0),(0,1,0),block*-15)
+        )
+
+show_object(big_block)
