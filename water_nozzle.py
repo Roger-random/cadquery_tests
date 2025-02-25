ea8d9116d8ebff64e1656fa097147722c4b4ec0a
Roger Cheng <roger.random@outlook.com>
2025-02-24 18:11:09 -0800

    Base and basic elbow nozzle

--- water_nozzle.py
+++ water_nozzle.py
@@ -31,43 +31,122 @@ and spoil the effect.
 """
 
 import cadquery as cq
+import cadquery.selectors as sel
 
 def water_nozzle_base(
     length = 95,
     diameter = 70,
-    threaded_exterior_size = 40,
+    threaded_exterior_size = 45,
     threaded_interior_diameter = 20.75,
     threaded_length = 20,
-    unthreaded_interior_diameter = 22.5,
-    lip_depth = 4,
-    lip_length = 4,
+    shell_thickness = 1.6,
+    lip_size = 4,
     ):
     threaded_end = (
         cq.Workplane("YZ")
         .circle(threaded_exterior_size/2)
         .circle(threaded_interior_diameter/2)
-        .extrude(threaded_length)
+        .extrude(-threaded_length)
     )
 
-    thread_transition_length = unthreaded_interior_diameter - threaded_interior_diameter
-    lip_section_length = lip_length*2 + lip_depth
-    lip_section = (
-        cq.Workplane("YZ")
-        .circle(threaded_exterior_size/2 - lip_depth)
-        .extrude(-lip_length)
-        .faces("<X").workplane()
-        .circle(threaded_exterior_size/2 - lip_depth)
-        .workplane(offset=lip_depth)
-        .circle(threaded_exterior_size/2)
-        .loft()
-        .faces("<X").workplane()
-        .circle(threaded_exterior_size/2)
-        .extrude(lip_length)
+    flared_end = (
+        cq.Workplane("XZ")
+        .lineTo(-threaded_length, threaded_exterior_size/2, forConstruction=True)
+        .bezier(
+            (
+                (-threaded_length, threaded_exterior_size/2),
+                (-threaded_length*3, threaded_exterior_size/2),
+                (-length+5, diameter/2 - 5),
+                (-length, diameter/2)
+            )
+        )
+        .lineTo(-length, diameter/2 - shell_thickness)
+        .bezier(
+            (
+                (-length, diameter/2 - shell_thickness),
+                (-length+ 5, diameter/2 - 5 - shell_thickness),
+                (-threaded_length*3, threaded_exterior_size/2 - shell_thickness),
+                (-threaded_length, threaded_exterior_size/2 - shell_thickness),
+                (-threaded_length, threaded_interior_diameter/2)
+            )
+        )
+        .close()
+        .revolve(360, (0,0,0),(1,0,0))
     )
 
-    base = threaded_end + lip_section
+    lip_cutout = (
+        cq.Workplane("XZ")
+        .lineTo(-lip_size*2, threaded_exterior_size/2 - lip_size*2, forConstruction=True)
+        .line(0,lip_size)
+        .line(lip_size*2, 0)
+        .line(0,lip_size*2)
+        .line(-lip_size*6,0)
+        .line(lip_size*3, -lip_size*3)
+        .close()
+        .revolve(360, (0,0,0),(1,0,0))
+    )
+
+    base = threaded_end + flared_end - lip_cutout
+
+    base = base.faces(">X").chamfer(0.5)
+    base = base.faces(">X[1]").chamfer(0.5)
 
     return base
 
+def mounting_clip(
+    base_diameter = 45,
+    lip_size = 4,
+    ):
+    clip_full = (
+        cq.Workplane("XZ")
+        .lineTo(0, base_diameter/2 - lip_size, forConstruction=True)
+        .line(0, lip_size)
+        .line(-lip_size*4,0)
+        .line(0,-lip_size)
+        .line(lip_size, -lip_size)
+        .line(lip_size, 0)
+        .line(0, lip_size)
+        .close()
+        .revolve(360, (0,0,0),(1,0,0))
+    )
+
+    clip_cut_half = (
+        cq.Workplane("XY")
+        .line(0, base_diameter/2-lip_size)
+        .line(-lip_size*2, 0)
+        .line(0, -lip_size)
+        .line(-lip_size, 0)
+        .line(-lip_size, lip_size)
+        .lineTo(-lip_size*4, 0)
+        .close()
+        .extrude(-base_diameter)
+    )
+
+    clip_cut = clip_cut_half + clip_cut_half.mirror("XZ")
+
+    clip = clip_full - clip_cut
+
+    clip = clip.edges("<Z").fillet(2)
+
+    return clip
+
+def simple_elbow(
+    base_diameter = 45,
+    inner_diameter = 15,
+    angle = 75,
+    ):
+
+    elbow = (
+        cq.Workplane("YZ")
+        .circle(base_diameter/2)
+        .circle(inner_diameter/2)
+        .revolve(angle, (0, -base_diameter*0.51, 0), (1, -base_diameter*0.51, 0))
+    )
+
+    elbow = elbow.edges(sel.NearestToPointSelector((0, inner_diameter/2,0))).fillet(2)
+
+    return mounting_clip()+elbow
+
 if 'show_object' in globals():
     show_object(water_nozzle_base(), options={"color":"blue", "alpha":0.5})
+    show_object(simple_elbow(), options={"color":"red", "alpha":0.5})
