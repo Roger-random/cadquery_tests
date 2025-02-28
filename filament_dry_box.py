a8d4200361e6a865a0e75ee0864767da276ca65f
Roger Cheng <roger.random@outlook.com>
2025-02-27 20:24:32 -0800

    Test lid says shell thickness 0.8 is too thin

--- filament_dry_box.py
+++ filament_dry_box.py
@@ -42,7 +42,7 @@ class filament_dry_box:
         spool_diameter_margin=3.4,
         spool_width=70,
         spool_width_margin=5,
-        shell_thickness=0.8,
+        shell_thickness=1.6,
         shell_bottom_radius=10,
         bottom_extra_height=40,
         lid_height=10,
