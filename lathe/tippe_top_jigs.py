179ed2276d948b26e1c5e89f55febd27314af0b6
Roger Cheng <roger.random@outlook.com>
2025-07-17 02:23:58 -0700

    Collet in a 5C Collet

--- lathe/tippe_top_jigs.py
+++ lathe/tippe_top_jigs.py
@@ -68,6 +68,9 @@ class tippe_top_jigs:
         # Lathe tool post is designed for cutters of this shank size
         self.tool_height = inch_to_mm(0.75)
 
+        # End of the tippe body ball
+        self.ball_end = -self.tippe_radius - self.quarter_sphere_groove_radius
+
         pass
 
     def tippe_body_round_exterior_lathe(self):
@@ -76,7 +79,6 @@ class tippe_top_jigs:
         on a CNC collet cut from bar stock.
         """
 
-        ball_end = -self.tippe_radius - self.quarter_sphere_groove_radius
         groove_end = (
             -self.tippe_radius - self.quarter_sphere_groove_radius - self.grip_groove
         )
@@ -87,7 +89,7 @@ class tippe_top_jigs:
             .radiusArc(
                 endPoint=(
                     self.quarter_sphere_groove_radius,
-                    ball_end,
+                    self.ball_end,
                 ),
                 radius=self.tippe_radius,
             )
@@ -110,9 +112,103 @@ class tippe_top_jigs:
             .revolve(angleDegrees=360, axisStart=(0, 0, 0), axisEnd=(0, 1, 0))
         )
 
+    def collet_collet(
+        self,
+        thickness=2.4,
+        collet_radius=inch_to_mm(0.5),
+        center_radius=inch_to_mm(0.375),
+        depth=inch_to_mm(1),
+        width=inch_to_mm(0.625),
+    ):
+        """
+        A ball is awkward to hold in a standard 5C collet. There are blanks
+        available for custom machined workholding called "emergency collets"
+        but before we commit to cutting one up we can prototype with a 3D-
+        printed collet that sits in a standard 5C collet.
+        """
+        revolve = (
+            cq.Workplane("ZX")
+            .radiusArc(
+                endPoint=(
+                    self.quarter_sphere_groove_radius,
+                    self.ball_end,
+                ),
+                radius=self.tippe_radius,
+            )
+            .line(thickness, 0)
+            .radiusArc(
+                endPoint=(self.tippe_radius + thickness, -self.tippe_radius),
+                radius=-self.tippe_radius,
+            )
+            .lineTo(self.tippe_radius + thickness, -thickness)
+            .radiusArc(endPoint=(collet_radius, 0), radius=-thickness)
+            .lineTo(collet_radius, depth - collet_radius * 0.25)
+            .radiusArc(
+                endPoint=(collet_radius * 0.75, depth), radius=-collet_radius / 4
+            )
+            .lineTo(0, depth)
+            .close()
+            .revolve(
+                angleDegrees=360,
+                axisStart=cq.Vector(0, 0, 0),
+                axisEnd=cq.Vector(0, 1, 0),
+            )
+        )
+
+        center = (
+            cq.Workplane("YZ").circle(radius=center_radius).extrude(depth - thickness)
+        ) + (
+            cq.Workplane("YZ").circle(radius=center_radius).extrude(-self.tippe_radius)
+        )
+
+        fastener = (
+            cq.Workplane("YZ")
+            .transformed(offset=(-center_radius / 2, 0))
+            .circle(radius=3.2 / 2)
+            .extrude(depth + 1)
+        )
+
+        radians120 = math.radians(120)
+        sin120 = math.sin(radians120)
+        cos120 = math.cos(radians120)
+        intersect_outer_y = -self.tippe_radius - thickness - 1
+        intersect_half = (
+            cq.Workplane("YZ")
+            .lineTo(width * cos120 / 2, width * sin120 / 2)
+            .lineTo(intersect_outer_y, width * sin120 / 2)
+            .lineTo(intersect_outer_y, 0)
+            .close()
+            .extrude(depth + 1, both=True)
+        )
+        intersect = intersect_half + intersect_half.mirror("XY")
+
+        leaf = (revolve - center - fastener).intersect(intersect)
+
+        disc = (
+            (
+                cq.Workplane("YZ")
+                .transformed(offset=(0, 0, depth - thickness))
+                .circle(radius=center_radius - 0.2)
+                .extrude(-thickness)
+            )
+            - fastener
+            - fastener.rotate(
+                axisStartPoint=(0, 0, 0), axisEndPoint=(1, 0, 0), angleDegrees=120
+            )
+            - fastener.rotate(
+                axisStartPoint=(0, 0, 0), axisEndPoint=(1, 0, 0), angleDegrees=240
+            )
+        )
+
+        return (leaf, disc)
+
 
 ttj = tippe_top_jigs()
 
 show_object(
     ttj.tippe_body_round_exterior_lathe(), options={"color": "green", "alpha": 0.5}
 )
+
+(leaf, disc) = ttj.collet_collet()
+show_object(leaf, options={"color": "red", "alpha": 0.5})
+show_object(disc, options={"color": "pink", "alpha": 0.5})
