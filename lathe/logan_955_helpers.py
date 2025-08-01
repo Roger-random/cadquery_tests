3c27143125111712bcb439bcbac8923b1a3c7a19
Roger Cheng <roger.random@outlook.com>
2025-08-01 13:56:43 -0700

    4-jaw ready for first draft, 3-jaw needs more math

--- lathe/logan_955_helpers.py
+++ lathe/logan_955_helpers.py
@@ -132,7 +132,79 @@ class logan_955_helpers:
 
         return tray
 
+    def chuck_removal_tool(self, jaws=3):
+        """
+        Visualize the volume for a tool that helps removing a chuck. It is
+        gripped by chuck jaws and presents a 7/8" hex for a wrench. 3D printed
+        plastic is not strong enough for the task so this is visualizing a
+        part before I make it in metal.
+
+        Variations for 3-jaw and 4-jaw chucks.
+        """
+        bar_stock_length = inch_to_mm(3)
+        bar_stock_radius = inch_to_mm(0.5)
+        bar_stock = (
+            cq.Workplane("XY")
+            .circle(radius=bar_stock_radius)
+            .extrude(bar_stock_length)
+            .faces("<Z or >Z")
+            .chamfer(2)
+        )
+
+        hex_size = inch_to_mm(7 / 8)
+        hex_length = inch_to_mm(0.75)
+        endmill_diameter = inch_to_mm(0.75)
+        endmill_removal_height = inch_to_mm(3)
+        endmill_removal_length = inch_to_mm(4)
+        endmill_removal = (
+            cq.Workplane("XY")
+            .circle(radius=endmill_diameter / 2)
+            .extrude(endmill_removal_height / 2, both=True)
+        ) + (
+            cq.Workplane("XY").box(
+                length=endmill_diameter,
+                width=endmill_removal_length,
+                height=endmill_removal_height,
+                centered=(True, False, True),
+            )
+        )
+
+        # Create hex
+        hex_mill = endmill_removal.rotate((0, 0, 0), (1, 0, 0), -90).translate(
+            ((hex_size + endmill_diameter) / 2, 0, hex_length)
+        )
+
+        for angle in range(0, 360, 60):
+            bar_stock = bar_stock - hex_mill.rotate((0, 0, 0), (0, 0, 1), angle)
+
+        # Cut jaw slots
+        jaw_length = inch_to_mm(1.5)
+        jaw_slot_cut_depth = inch_to_mm(0.3)
+        depth_to_leave = bar_stock_radius - jaw_slot_cut_depth
+        jaw_mill = endmill_removal.rotate((0, 0, 0), (1, 0, 0), 90).translate(
+            (
+                depth_to_leave + endmill_diameter / 2,
+                depth_to_leave + endmill_removal_height / 2,
+                bar_stock_length - jaw_length,
+            )
+        )
+
+        if jaws == 3:
+            jaw_mill = jaw_mill + jaw_mill.mirror("XZ")
+            jaw_mill = jaw_mill.translate((0, 0, 0))
+            for angle in range(0, 360, 120):
+                bar_stock = bar_stock - jaw_mill.rotate((0, 0, 0), (0, 0, 1), angle)
+        elif jaws == 4:
+            for angle in range(0, 360, 90):
+                bar_stock = bar_stock - jaw_mill.rotate((0, 0, 0), (0, 0, 1), angle)
+        else:
+            raise ValueError(f"Unsupported jaw count {jaws}")
+
+        return bar_stock
+
 
 helpers = logan_955_helpers()
 
-show_object(helpers.oil_tray(), options={"color": "green", "alpha": 0.5})
+show_object(
+    helpers.chuck_removal_tool(jaws=4), options={"color": "green", "alpha": 0.5}
+)
