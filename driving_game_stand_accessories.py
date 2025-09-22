e921f065712da9a97ae03e26b7bd7f0bcdad864d
Roger Cheng <roger.random@outlook.com>
2025-09-21 19:35:26 -0700

    Math is getting too complex. Take a snapshot here before retry.

--- driving_game_stand_accessories.py
+++ driving_game_stand_accessories.py
@@ -50,47 +50,67 @@ class driving_game_stand_accessories:
         self.beam_side = inch_to_mm(1.0)
         self.print_margin = 0.1
 
+    def beam_transform(self, beam, width, thickness):
+        beam_rotate = 10
+        return (
+            beam.translate((0, 0, thickness))
+            .rotate(
+                (0, self.beam_side + thickness * 2, thickness),
+                (1, self.beam_side + thickness * 2, thickness),
+                beam_rotate,
+            )
+            .translate(
+                (
+                    width / 2 - self.beam_side / 2 - thickness,
+                    0,
+                    0,
+                )
+            )
+        )
+
     def shifter(self):
         mounting_holes_space = inch_to_mm(1.9)
         thickness = 4
         base_width = 60
         base_depth = 80
-        beam_rotate = 30
-        beam_translate = (
-            base_width / 2 - self.beam_side / 2 - thickness,
-            0,
-            (mounting_holes_space / 2) + self.beam_side / 2,
-        )
 
         beam_bracket_outer = cq.Workplane("XY").box(
             self.beam_side + thickness * 2,
             self.beam_side + thickness * 2,
-            mounting_holes_space + self.beam_side * 2.1,
+            mounting_holes_space + self.beam_side,
+            centered=(True, True, False),
         )
 
-        beam_channel = (
-            cq.Workplane("XY")
-            .box(
-                self.beam_side + self.print_margin,
-                self.beam_side + self.print_margin + thickness,
-                mounting_holes_space * 4,
-            )
-            .translate((0, thickness, 0))
-            .rotate((0, 0, 0), (1, 0, 0), beam_rotate)
-            .translate(beam_translate)
+        beam_channel = self.beam_transform(
+            (
+                cq.Workplane("XY")
+                .box(
+                    self.beam_side + self.print_margin,
+                    self.beam_side + self.print_margin + thickness,
+                    mounting_holes_space * 6,
+                    centered=(True, True, True),
+                )
+                .translate((0, thickness, 0))
+            ),
+            base_width,
+            thickness,
         )
 
         fastener_hole = (
             cq.Workplane("YZ")
-            .transformed(offset=(0, mounting_holes_space / 2, 0))
+            .transformed(offset=(0, self.beam_side / 2, 0))
             .circle(radius=3 + self.print_margin)
             .extrude(self.beam_side, both=True)
         )
 
-        beam_bracket = (
-            (beam_bracket_outer - fastener_hole - fastener_hole.mirror("XY"))
-            .rotate((0, 0, 0), (1, 0, 0), beam_rotate)
-            .translate(beam_translate)
+        beam_bracket = self.beam_transform(
+            (
+                beam_bracket_outer
+                - fastener_hole
+                - fastener_hole.translate((0, 0, mounting_holes_space))
+            ),
+            base_width,
+            thickness,
         )
 
         shifter_base = cq.Workplane("XY").box(
@@ -106,7 +126,14 @@ class driving_game_stand_accessories:
             )
             .translate((0, 0, -self.beam_side / 2))
         )
-        shifter_mount = shifter_base + beam_bracket - beam_channel - bracket_trim
+
+        bracket_edge_y = base_depth / 2 - self.beam_side / 2 - thickness
+        shifter_mount = (
+            shifter_base
+            + beam_bracket.translate((0, bracket_edge_y, 0))
+            - beam_channel.translate((0, bracket_edge_y, 0))
+            - bracket_trim
+        )
 
         return shifter_mount
 
